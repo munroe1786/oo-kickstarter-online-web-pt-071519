33a6f9615fe089573153d487dfe3b52f904d82d3
Eliza Munroe <era3munroe@gmail.com>
2019-08-18 21:09:27 +0000

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,18 @@
+class Project
+  
+  attr_accessor :backers
+  
+  attr_reader :title 
+  
+  def initialize(title)
+    @title = title
+    @backers = [ ] 
+  end
+  
+  def add_backer(backer)
+    @backers << backer
+    if !backer.backed_projects.include?(self)
+      backer.backed_projects << self
+    end
+  end
+end
